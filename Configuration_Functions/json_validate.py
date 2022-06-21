a9a22e8497a7260053f358c481d50e2ad8c1744e
Shivam Sharma <sshivam95@gmail.com>
2022-06-21 17:00:30 +0200

    Reformat: Reformat code

--- Configuration_Functions/json_validate.py
+++ Configuration_Functions/json_validate.py
@@ -1,4 +1,5 @@
 import json
+
 import jsonschema
 from jsonschema import validate
 
@@ -14,16 +15,16 @@ with open("paramSchema.json", "r") as f:
 schemata = json.loads(schema)
 
 
-def validateJson(jsonData):
+def validate_json(json_data):
     try:
-        validate(instance=jsonData, schema=schemata)
+        validate(instance=json_data, schema=schemata)
     except jsonschema.exceptions.ValidationError as err:
         return False
     return True
 
 
 for i in range(len(paramNames)):
-    isValid = validateJson(params[paramNames[i]])
+    isValid = validate_json(params[paramNames[i]])
     if isValid:
         print("Yes")
     if not isValid:
@@ -32,24 +33,24 @@ for i in range(len(paramNames)):
 
 """
 # Convert json to python object.
-jsonData = json.loads('{"name": "jane doe", "rollnumber": "25", "marks": 72}')
+json_data = json.loads('{"name": "jane doe", "rollnumber": "25", "marks": 72}')
 # validate it
-isValid = validateJson(jsonData)
+isValid = validate_json(json_data)
 if isValid:
-    print(jsonData)
+    print(json_data)
     print("Given JSON data is Valid")
 else:
-    print(jsonData)
+    print(json_data)
     print("Given JSON data is InValid")
 
 # Convert json to python object.
-jsonData = json.loads('{"name": "jane doe", "rollnumber": 25, "marks": 72}')
+json_data = json.loads('{"name": "jane doe", "rollnumber": 25, "marks": 72}')
 # validate it
-isValid = validateJson(jsonData)
+isValid = validate_json(json_data)
 if isValid:
-    print(jsonData)
+    print(json_data)
     print("Given JSON data is Valid")
 else:
-    print(jsonData)
+    print(json_data)
     print("Given JSON data is InValid")
 """
