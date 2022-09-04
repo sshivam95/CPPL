13f03d0d17182d698cf6a8ff8ef90630a31e6d8d
Shivam Sharma <sshivam95@gmail.com>
2022-09-04 12:25:34 +0200

    Add: Add upper_confidence_bound.py

--- preselection/upper_confidence_bound.py
+++ preselection/upper_confidence_bound.py
@@ -1,3 +1,17 @@
 class UCB:
-    def __init__(self, subset, features, ):
+    def __init__(
+            self,
+            S_t,
+            X_t,
+            d,
+            grad,
+            grad_op_sum,
+            hess_sum,
+            subset_size,
+            n,
+            omega,
+            theta_bar,
+            time_step,
+            v_hat
+    ):
         pass
