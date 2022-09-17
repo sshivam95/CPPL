e223b757076c03c35103e82edfd4549126a866d9
shivam <sshivam95@gmail.com>
2022-09-17 13:06:53 +0200

    Add: Add init file for preselection module

---
+++ preselection/__init__.py
@@ -0,0 +1,8 @@
+from preselection.upper_confidence_bound import UCB
+
+regret_minimizing_algorithm = [
+    UCB
+]
+__all__ = (
+    [preselection_algorithm.__name__ for preselection_algorithm in regret_minimizing_algorithm]
+)
